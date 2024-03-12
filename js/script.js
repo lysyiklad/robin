8628edd94cff3d92a29a77a2e368512ea1780e57
lysyiklad <vadim.klad@gmail.com>
2024-03-12 15:40:59 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -6,6 +6,12 @@ let flagScroolHeader = false;
 let curIndexGallery = 1;
 let isOpenMenu = false;
 
+const btnHdr1 = document.getElementById('btn-hdr-1');
+const btnHdr2 = document.getElementById('btn-hdr-2');
+const btnHdr3 = document.getElementById('btn-hdr-3');
+const btnHdr4 = document.getElementById('btn-hdr-4');
+// console.log(btnHdr1);
+
 for (let index = 1; index <= 4; index++) {
   let btn = document.getElementById(`btn-${index}`);
   let imgGallery = document.getElementById(`img-gallery-${index}`);
@@ -54,10 +60,33 @@ window.addEventListener('scroll', function () {
     header.classList.remove('bg-transparent');
     header.classList.add('bg-black');
     // addBgBlackHdr();
-
     flagScroolHeader = true;
   } else if (window.scrollY <= heightHeader) {
     header.classList.add('bg-transparent');
     flagScroolHeader = false;
+  }
+
+  if (window.scrollY > 700 && window.scrollY <= 1400) {
+    btnHdr1.classList.add('bg-[#5221E6]');
+  } else {
+    btnHdr1.classList.remove('bg-[#5221E6]');
+  }
+
+  if (window.scrollY > 2700 && window.scrollY <= 4600) {
+    btnHdr2.classList.add('bg-[#5221E6]');
+  } else {
+    btnHdr2.classList.remove('bg-[#5221E6]');
+  }
+
+  if (window.scrollY > 8600 && window.scrollY <= 9000) {
+    btnHdr3.classList.add('bg-[#5221E6]');
+  } else {
+    btnHdr3.classList.remove('bg-[#5221E6]');
+  }
+
+  if (window.scrollY > 9800) {
+    btnHdr4.classList.add('bg-[#5221E6]');
+  } else {
+    btnHdr4.classList.remove('bg-[#5221E6]');
   }
 });
